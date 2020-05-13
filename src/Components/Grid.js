8a6ef4da864c55978afa72bc50ca91a93d00b4a1
Lauren C <48397414+laurloves2code@users.noreply.github.com>
2020-05-13 13:41:24 -0400

    numbers array added

--- src/Components/Grid.js
+++ src/Components/Grid.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react';
+import data from './numberarray.json';
 
 class Grid extends Component {
-
     
+    state = {
+        block1: '',
+        block2: '',
+        block3: ''
+    };
+    
+    componentDidMount() {
+        this.displayNumbers();
+    }
+    
+    displayNumbers = () => {
+        const numbersItem = data.numbergrid;
+        console.log(numbersItem);  
+        this.setState({block1: numbersItem[0].number});
+        this.setState({block2: numbersItem[1].number});
+        this.setState({block3: numbersItem[2].number});      
+    }
 
     render () {
         return (
             <div>
                 <h1>Number Grid</h1>
                 <div className="grid-container">
-                    <div className="grid-item">1</div>
-                    <div className="grid-item">2</div>
-                    <div className="grid-item">3</div>
+                    <div className="grid-item">{this.state.block1}</div>
+                    <div className="grid-item">{this.state.block2}</div>
+                    <div className="grid-item">{this.state.block3}</div>
                     <div className="grid-item">4</div>
                     <div className="grid-item">5</div>
                     <div className="grid-item">6</div>
